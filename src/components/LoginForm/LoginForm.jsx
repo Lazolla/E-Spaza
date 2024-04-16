d52a32f591239261982ff4c15d6d93701594b782
KoketsoMhlarhi <2433734@students.wits.ac.za>
2024-04-16 22:53:47 +0200

    add default register Link

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -23,12 +23,12 @@ function LoginForm() {
             
             <section className='remember-forgot'>
                 <label><input type="checkbox" />Remember me</label>
-                <a href="#">Forgot password?</a>
+                <a href="https://www.aautomate.com/images/easyblog_shared/May_2021/5-21-21/b2ap3_large_165017799_password_400.jpg">Forgot password?</a>
             </section>
             
             <button type='submit'>Login</button>
             <article className='register-link' >
-                <p>Don't have an account? <a href="#">Register</a></p>
+                <p>Don't have an account? <a href="https://thumbs.dreamstime.com/b/register-now-concept-button-web-site-hand-holds-wooden-tablet-text-107028457.jpg">Register</a></p>
             </article >
             
         </form>
